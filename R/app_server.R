c276b11e0ea3373dc195503305e72fd45029ebfa
André Bittermann <42348155+abitter@users.noreply.github.com>
2022-12-30 11:59:23 +0100

    Update Dec 22

--- R/app_server.R
+++ R/app_server.R
@@ -10,7 +10,7 @@ app_server <- function( input, output, session ) {
   
   
   ## update the following line by hand
-  r$last_updated = "September 13, 2022"
+  r$last_updated = "December 30, 2022"
   
   
   r$n_docs_year <- isolate(readRDS("inst/data/n_docs_year.RDS"))
