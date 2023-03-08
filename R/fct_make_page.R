ca47a36e5aca8da4d1fdf06c06e65f07bca66e25
pplanger <126692514+pplanger@users.noreply.github.com>
2023-03-08 16:59:12 +0100

    Update fct_make_page.R

--- R/fct_make_page.R
+++ R/fct_make_page.R
@@ -60,7 +60,7 @@ header <- tagList(
     items = tagList(
 
       
-      CommandBarItem(text = tags$img(src = "www/logo.png", class = "logo"), href = "https://www.leibniz-psychology.org/", target = "_blank"),
+      CommandBarItem(text = tags$img(src = "www/logo.png", class = "logo"), href = "https://www.leibniz-psychology.org/", target = "_blank")
     ),
 
       items = list(
