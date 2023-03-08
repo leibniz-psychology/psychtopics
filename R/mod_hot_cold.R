c8b00f7b07256b2ae1c84bc580aee14cd73c8885
pplanger <126692514+pplanger@users.noreply.github.com>
2023-03-08 12:08:59 +0100

    Update mod_hot_cold.R

--- R/mod_hot_cold.R
+++ R/mod_hot_cold.R
@@ -213,7 +213,7 @@ mod_hot_cold_server <- function(id, r){
       req(r$current_year, r$start_year, opened())
       
       shiny.fluent::Slider(
-        onChange = shiny.fluent::setInput(ns("slider"), 2),
+        #onChange = shiny.fluent::setInput(ns("slider"), 2),
         ranged = TRUE,
         label = "Select the range of years",
         min = r$start_year,
