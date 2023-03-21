34c8b4cca92ac12799a5a47012a81bd68d2e6c63
pplanger <126692514+pplanger@users.noreply.github.com>
2023-03-21 14:23:01 +0100

    Update fct_make_page.R

--- R/fct_make_page.R
+++ R/fct_make_page.R
@@ -15,6 +15,7 @@ library(shiny.router)
 makePage <- function (title, subtitle, contents) {
   tagList(div(
     class = "page-title",
+    horizontalAlign = "center",
     span(title, class = "ms-fontSize-32 ms-fontWeight-semibold", style = "color: #323130"),
     span(subtitle, class = "ms-fontSize-14 ms-fontWeight-regular", style = "color: #605E5C; margin: 0px;")
   ),      
