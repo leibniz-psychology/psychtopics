9ef76b9418f21d7d8bcd7be95c224bb62461253d
pplanger <126692514+pplanger@users.noreply.github.com>
2023-03-21 15:18:23 +0100

    Update fct_make_page.R

--- R/fct_make_page.R
+++ R/fct_make_page.R
@@ -12,15 +12,15 @@ library(shiny.fluent)
 library(glue)
 library(shiny.router)
 
-#makePage <- function (title, subtitle, contents) {
-#  tagList(div(
-#    class = "page-title",
-#    #align = "center",
-#    span(title, class = "ms-fontSize-32 ms-fontWeight-semibold", style = "color: #323130"),
-#    span(subtitle, class = "ms-fontSize-14 ms-fontWeight-regular", style = "color: #605E5C; margin: 14px;")
-#  ),      
-#  contents)
-#}
+makePage <- function (title, subtitle, contents) {
+  tagList(div(
+    class = "page-title",
+    #align = "center",
+    span(title, class = "ms-fontSize-32 ms-fontWeight-semibold", style = "color: #323130"),
+    span(subtitle, class = "ms-fontSize-14 ms-fontWeight-regular", style = "color: #605E5C; margin: 14px;")
+  ),      
+  contents)
+}
 
 makeCard <- function(title, content, size = 12, style = "") {
   div(
@@ -174,25 +174,25 @@ layout <- function(mainUI){
   )
 }
 
-card1 <- makeCard(
-  "Welcome to shiny.fluent demo!",
-  div(
-    Text("shiny.fluent is a package that allows you to build Shiny apps using Microsoft's Fluent UI."),
-    Text("Use the menu on the left to explore live demos of all available components.")
-  ))
+#card1 <- makeCard(
+#  "Welcome to shiny.fluent demo!",
+#  div(
+#    Text("shiny.fluent is a package that allows you to build Shiny apps using Microsoft's Fluent UI."),
+#    Text("Use the menu on the left to explore live demos of all available components.")
+#  ))
 
-card2 <- makeCard(
-  "shiny.react makes it easy to use React libraries in Shiny apps.",
-  div(
-    Text("To make a React library convenient to use from Shiny, we need to write an R package that wraps it - for example, a shiny.fluent package for Microsoft's Fluent UI, or shiny.blueprint for Palantir's Blueprint.js."),
-    Text("Communication and other issues in integrating Shiny and React are solved and standardized in shiny.react package."),
-    Text("shiny.react strives to do as much as possible automatically, but there's no free lunch here, so in all cases except trivial ones you'll need to do some amount of manual work. The more work you put into a wrapper package, the less work your users will have to do while using it.")
-  ))
+#card2 <- makeCard(
+#  "shiny.react makes it easy to use React libraries in Shiny apps.",
+#  div(
+#    Text("To make a React library convenient to use from Shiny, we need to write an R package that wraps it - for example, a shiny.fluent package for Microsoft's Fluent UI, or shiny.blueprint for Palantir's Blueprint.js."),
+#    Text("Communication and other issues in integrating Shiny and React are solved and standardized in shiny.react package."),
+#    Text("shiny.react strives to do as much as possible automatically, but there's no free lunch here, so in all cases except trivial ones you'll need to do some amount of manual work. The more work you put into a wrapper package, the less work your users will have to do while using it.")
+# ))
 
-home_page <- makePage(
-  "This is a Fluent UI app built in Shiny",
-  "shiny.react + Fluent UI = shiny.fluent",
-  div(card1, card2)
-)
+#home_page <- makePage(
+#  "This is a Fluent UI app built in Shiny",
+#  "shiny.react + Fluent UI = shiny.fluent",
+#  div(card1, card2)
+#)
 
 
