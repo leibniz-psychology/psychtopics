4decfa6883d0d70cd5804a937d21010abf1b06c7
pplanger <126692514+pplanger@users.noreply.github.com>
2023-03-21 15:16:46 +0100

    Update fct_make_page.R

--- R/fct_make_page.R
+++ R/fct_make_page.R
@@ -12,15 +12,15 @@ library(shiny.fluent)
 library(glue)
 library(shiny.router)
 
-makePage <- function (title, subtitle, contents) {
-  tagList(div(
-    class = "page-title",
-    #align = "center",
-    span(title, class = "ms-fontSize-32 ms-fontWeight-semibold", style = "color: #323130"),
-    span(subtitle, class = "ms-fontSize-14 ms-fontWeight-regular", style = "color: #605E5C; margin: 14px;")
-  ),      
-  contents)
-}
+#makePage <- function (title, subtitle, contents) {
+#  tagList(div(
+#    class = "page-title",
+#    #align = "center",
+#    span(title, class = "ms-fontSize-32 ms-fontWeight-semibold", style = "color: #323130"),
+#    span(subtitle, class = "ms-fontSize-14 ms-fontWeight-regular", style = "color: #605E5C; margin: 14px;")
+#  ),      
+#  contents)
+#}
 
 makeCard <- function(title, content, size = 12, style = "") {
   div(
