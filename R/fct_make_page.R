d9bc18630f9248258fd007a1105853ee98a90e53
pplanger <126692514+pplanger@users.noreply.github.com>
2023-03-21 13:59:40 +0100

    Update fct_make_page.R

--- R/fct_make_page.R
+++ R/fct_make_page.R
@@ -16,7 +16,7 @@ makePage <- function (title, subtitle, contents) {
   tagList(div(
     class = "page-title",
     span(title, class = "ms-fontSize-32 ms-fontWeight-semibold", style = "color: #323130"),
-    span(subtitle, class = "ms-fontSize-14 ms-fontWeight-regular", style = "color: #605E5C; margin: 14px;")
+    span(subtitle, class = "ms-fontSize-14 ms-fontWeight-regular", style = "color: #605E5C; margin: 0px;")
   ),
   contents)
 }
