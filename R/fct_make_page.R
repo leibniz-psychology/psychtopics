0299063ccdbc39112336c10773c63fc0402a578a
pplanger <126692514+pplanger@users.noreply.github.com>
2023-03-21 11:23:50 +0100

    Update fct_make_page.R

--- R/fct_make_page.R
+++ R/fct_make_page.R
@@ -104,7 +104,7 @@ navigation <- Nav(
   initialSelectedKey = 'home',
   styles = list(
     linkText = list(
-      margin = 1px,
+      margin = "1px",
       fontWeight = 600,
       fontSize = 15,
       color = "white",
