f89c79a7d15f6aec196502d0d2d20a63630bed3b
pplanger <126692514+pplanger@users.noreply.github.com>
2023-03-21 10:33:48 +0100

    Update fct_make_page.R

--- R/fct_make_page.R
+++ R/fct_make_page.R
@@ -82,7 +82,7 @@ header <- tagList(
 )
 
 title2 = tagList(
-  div("PsychTopics", class = "ms-fontSize-32 ms-fontWeight-semibold", style = "color: #fff; font-size: 3rem; padding-left: 33px"),
+  div("PsychTopics", class = "ms-fontSize-32 ms-fontWeight-semibold", style = "color: #fff; font-size: 3rem"),
 )
 
 #title2 = tagList(
