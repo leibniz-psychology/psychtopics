3d1eac096a505862b1243a69a79a7b37e040a33a
pplanger <126692514+pplanger@users.noreply.github.com>
2023-03-22 16:15:48 +0100

    Update mod_popular_by_year.R

--- R/mod_popular_by_year.R
+++ R/mod_popular_by_year.R
@@ -92,7 +92,7 @@ mod_popular_by_year_ui <- function(id){
           
           echarts4r::echarts4rOutput(ns("plot_box2"), height = 430)
         )
-      )
+  #    )
     ),          
       
    
