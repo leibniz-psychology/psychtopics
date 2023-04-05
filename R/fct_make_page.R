d5510190938ada4a4a4f7318c382344f81feb3a9
pplanger <126692514+pplanger@users.noreply.github.com>
2023-04-05 15:17:03 +0200

    Update fct_make_page.R

--- R/fct_make_page.R
+++ R/fct_make_page.R
@@ -70,9 +70,9 @@ header <- tagList(
   
   CommandBar(
     items = tagList(
-      CommandBarItem(text = tags$img(src = "www/logo.png", class = "logo"), href = "https://www.leibniz-psychology.org/", target = "_blank",
-                    styles = list(root = list(fontWeight = 600, fontSize = 15))),
-      CommandBarItem("Start", href = '#!/', target = "_self", key = 'home'),
+      CommandBarItem(text = tags$img(src = "www/logo.png", class = "logo"), href = "https://www.leibniz-psychology.org/", target = "_blank"),
+      CommandBarItem("Start", href = '#!/', target = "_self", key = 'home',
+                     styles = list(root = list(fontWeight = 600, fontSize = 15))),
       CommandBarItem("Browse Topics", href = '#!/browse-topics', target = "_self", key = 'browse'),
       CommandBarItem("Popular by Year", href = '#!/popular', target = "_self", key = 'popular'),
       #CommandBarItem("Compare Years", href = '#!/compare', target = "_self", key = 'compare'),
