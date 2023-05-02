098638364f83e2d76abca7df6067b6395c2388f1
pplanger <126692514+pplanger@users.noreply.github.com>
2023-05-02 10:14:28 +0200

    Update fct_make_page.R

--- R/fct_make_page.R
+++ R/fct_make_page.R
@@ -197,15 +197,6 @@ layout <- function(mainUI){
   )
 }
 
-#use_gotop()
-
-spsGoTop(
-  id = "gotop",
-  icon = NULL,
-  right = "1rem",
-  bottom = "10rem",
-  color = "#337ab7"
-)
 
 #card1 <- makeCard(
 #  "Welcome to shiny.fluent demo!",
