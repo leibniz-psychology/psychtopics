fda2ced570d999e8ac7ddfaa7a34149e34c2a30a
pplanger <126692514+pplanger@users.noreply.github.com>
2023-05-02 11:40:21 +0200

    Update mod_topic_evol.R

--- R/mod_topic_evol.R
+++ R/mod_topic_evol.R
@@ -128,13 +128,13 @@ mod_topic_evol_ui <- function(id){
         )
       )
     ),
-  spsGoTop(
-    id = "gotop",
-    icon = NULL,
-    right = "1rem",
-    bottom = "10rem",
-    color = "#337ab7"
-    )   
+spsGoTop(
+  id = "gotop",
+  icon = icon("arrow-up-long", "fa-solid"),
+  right = "2rem",
+  bottom = "5rem",
+  color = "#953386"
+)  
   )
 }
     
