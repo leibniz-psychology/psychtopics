37a412a6e6e2df810f335752e3b5aa828898fa0a
pplanger <126692514+pplanger@users.noreply.github.com>
2023-05-02 09:29:23 +0200

    Update fct_make_page.R

--- R/fct_make_page.R
+++ R/fct_make_page.R
@@ -7,11 +7,10 @@
 #' @return The return value, if any, from executing the function.
 #'
 #' @noRd
-install.packages("gotop")
 library(shiny.fluent)
 library(glue)
 library(shiny.router)
-library(gotop)
+
 
 #makePage <- function (title, subtitle, contents) {
 #  tagList(div(
