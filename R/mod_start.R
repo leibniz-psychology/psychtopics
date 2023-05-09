253554b9929f205cd1d86677937bb3a160388f04
pplanger <126692514+pplanger@users.noreply.github.com>
2023-05-09 11:18:44 +0200

    Update mod_start.R

--- R/mod_start.R
+++ R/mod_start.R
@@ -120,7 +120,7 @@ mod_start_ui <- function(id){
       style = "margin-bottom: 0;",
       makeCard(
         size = 12,
-        style = "background-color: #c6cf78ff",
+        style = "background-color: #999999",
         title = title_with_help(
           id = ns("help2"),
           title = uiOutput(ns("title_box3")),
