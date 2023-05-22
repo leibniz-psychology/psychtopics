8a78006c095798460a4565a16a9bb9bf441e9c84
pplanger <126692514+pplanger@users.noreply.github.com>
2023-05-22 15:31:23 +0200

    Update mod_popular_by_year.R

--- R/mod_popular_by_year.R
+++ R/mod_popular_by_year.R
@@ -410,8 +410,8 @@ mod_popular_by_year_server <- function(id, r){
               year = vals[1];
               min_year = vals[3];
               top_terms = year <= min_year ? vals[0].match(min_year + '.*')[0].replace(min_year, '') : vals[0].match(year + '.*')[0].replace(year, '');
-              return('ID: ' + params.value[1] + 
-                      '<br/> Label: ' + vals[2] +
+              // 'ID: ' + params.value[1] + 
+              return('Label: ' + vals[2] +
                       '<br/> Essential Publications: ' + params.value[0]) +
                       '<br/> Year: ' + year + 
                       '<br/> Evolution Terms' + top_terms
