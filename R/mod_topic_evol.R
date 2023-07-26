47cc400415a02fa67975dadfb794714e4b99aff3
pplanger <126692514+pplanger@users.noreply.github.com>
2023-07-26 10:57:17 +0200

    Update mod_topic_evol.R

--- R/mod_topic_evol.R
+++ R/mod_topic_evol.R
@@ -351,7 +351,8 @@ mod_topic_evol_server <- function(id, r){
         defaultColDef = reactable::colDef(
           html = TRUE,
           rowHeader = TRUE,
-          headerStyle = list(backgroundColor = "#dddddd")
+          headerStyle = list(backgroundColor = "#dddddd"),
+          columnRule = "4px solid #000000",
           #style = list(columnRule = "4px solid #000000")
         ),
         rownames = FALSE,
