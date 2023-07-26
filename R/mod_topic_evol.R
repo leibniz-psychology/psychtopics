60e1070bf970b7562f13f5d05511a0021e4784f2
pplanger <126692514+pplanger@users.noreply.github.com>
2023-07-26 09:37:51 +0200

    Update mod_topic_evol.R

--- R/mod_topic_evol.R
+++ R/mod_topic_evol.R
@@ -368,7 +368,7 @@ mod_topic_evol_server <- function(id, r){
         # ),
         theme = reactable::reactableTheme(
           rowSelectedStyle = list(backgroundColor = "#c6cf78ff", boxShadow = "inset 2px 0 0 0 #ffa62d"),
-          borderColor = "#5b5b5b"
+          borderColor = "#999999"
         )
         
         # columns = list(
