46bfa85ac7e97436683edd70bff3dc4a8a83715b
pplanger <126692514+pplanger@users.noreply.github.com>
2023-07-26 10:46:47 +0200

    Update mod_topic_evol.R

--- R/mod_topic_evol.R
+++ R/mod_topic_evol.R
@@ -352,7 +352,7 @@ mod_topic_evol_server <- function(id, r){
           html = TRUE,
           rowHeader = TRUE,
           headerStyle = list(backgroundColor = "#dddddd"),
-          style = list(column-rule = "4px solid #000000")
+          style = list(columnrule = "4px solid #000000")
         ),
         rownames = FALSE,
         compact = TRUE,
